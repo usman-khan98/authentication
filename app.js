ef586eaae06810d246b4911f643c836fb753c262
usman <usman544k@gmail.com>
2022-04-28 21:44:39 +0500

    added hashing level4

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-require('dotenv').config()
+//require('dotenv').config()
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const express = require('express');
-const encrypt = require('mongoose-encryption');
+//const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -16,7 +17,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema);
 
@@ -32,7 +33,7 @@ app.get("/login", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const pass = req.body.password;
+  const pass = md5(req.body.password);
   User.findOne({email: username}, function(err, result){
     if (err) {
       res.send("There was an error")
@@ -56,7 +57,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if (err) {
