2b8986e02613d7c621e985b9839ff79146842a15
usman <usman544k@gmail.com>
2022-04-28 22:50:43 +0500

    added bcrypt package and changed code level 5

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
-//require('dotenv').config()
+require('dotenv').config()
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const express = require('express');
-//const encrypt = require('mongoose-encryption');
+const encrypt = require('mongoose-encryption');
 const md5 = require('md5');
+const bcrypt = require('bcrypt');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -17,7 +18,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+// To see previous levels Check github repository commits section
 
 const User = mongoose.model("User", userSchema);
 
@@ -33,16 +34,23 @@ app.get("/login", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const pass = md5(req.body.password);
-  User.findOne({email: username}, function(err, result){
+  const pass = req.body.password;
+  User.findOne({email: username}, function(err, foundUser){
     if (err) {
       res.send("There was an error")
     } else {
-      if (pass === result.password) {
-        res.render("secrets");
+      if (foundUser) {
+        bcrypt.compare(pass, foundUser.password, function(err1, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+          else{
+            res.send("<h1>Incorrect Password</h1>");
+          }
+        });
       }
-      else{
-        res.send("<h1>Incorrect Username/Password</h1>");
+      else {
+        res.send("<h1>Incorrect Username</h1>");
       }
     }
   })
@@ -55,17 +63,21 @@ app.get("/register", function(req, res){
 
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+  bcrypt.hash(req.body.password, 12, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err) {
+        res.send("There was an error registering you");
+      } else {
+        res.render("secrets");
+      }
+    })
+
   });
-  newUser.save(function(err){
-    if (err) {
-      res.send("There was an error registering you");
-    } else {
-      res.render("secrets");
-    }
-  })
+
 })
 
 app.get("/logout", function(req, res){
